49e8fc7a90476a5c0f35f2e721db2dc81f89fd59
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2019-10-12 12:49:05 +0545

    Initial release

---
+++ automate-wp-posts.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Plugin Name: Automate WP Posts
+ * Description: Send post data to online automation tools with Webhooks
+ * Version: 1.0.0
+ * Author: Sanjeev Aryal
+ * Author URI: http://www.sanjeebaryal.com.np
+ * Text Domain: automate-wp-posts
+ */
+
+defined( 'ABSPATH' ) or die( "No script kiddies please!" );
+
+add_action( 'admin_menu', 'awp_register_setting_menu' );
+add_action( 'admin_init', 'awp_save_settings' );
+add_action( 'publish_post', 'awp_publish_post', 10, 2 );
+
+/**
+ * Add Automate WP Posts Submenu
+ *
+ * @since  1.0.0
+ */
+function awp_register_setting_menu() {
+	add_options_page( 'Automate WP Posts', 'Automate WP Posts', 'manage_options', 'automate-wp-posts', 'awp_settings_page' );
+}
+
+/**
+ * Settings page for Automate WP Posts
+ * 
+ * @since 1.0.0
+ */
+function awp_settings_page() {
+
+	$webhook_url = get_option( 'awp_webhook_url' );
+	?>
+		<h2 class="wp-heading-inline"><?php esc_html_e( 'Automate WP Posts Settings', 'automate-wp-posts' ); ?></h2>
+		<form method="post">
+			<table class="form-table">
+			        <tr valign="top">
+			           	<th scope="row"><?php echo esc_html__( 'Webhook URL', 'automate-wp-posts' );?></th>
+			        		<td><input style="width:35%" type="url" name="webhook_url" value ="<?php echo esc_url( $webhook_url ); ?>" class="automate-wp-posts-webhook-url" /><br/>
+			        		</td>
+			        </tr>
+			        <?php do_action( 'automate_wp_posts_settings' );?>
+		            <?php wp_nonce_field( 'automate_wp_posts_settings', 'automate_wp_posts_settings_nonce' );?>
+
+			</table>
+			    <?php submit_button(); ?>
+		</form>
+	<?php
+}
+
+/**
+ * Save Settings.
+ *
+ * @since 1.0.0
+ */
+function awp_save_settings() {
+
+	if( isset( $_POST['automate_wp_posts_settings_nonce'] ) ) {
+		if( ! wp_verify_nonce( $_POST['automate_wp_posts_settings_nonce'], 'automate_wp_posts_settings' )
+			) {
+			   print 'Nonce Failed!';
+			   exit;
+		} else {
+			$webhook_url = isset( $_POST['webhook_url'] ) ? esc_url_raw( $_POST['webhook_url'] ) : '';
+			$message     = esc_html__( 'Done!', 'automate-wp-posts');
+			$class       = 'notice-success';
+
+			update_option( 'awp_webhook_url', $webhook_url );
+
+			if ( filter_var( $webhook_url, FILTER_VALIDATE_URL ) === FALSE) {
+				$message = esc_html__( 'Not a valid webhook URL.' );
+				$class   = 'error';
+			}
+
+			add_action( 'admin_notices', function() use ( $message, $class ) {
+			    ?>
+				    <div class="notice <?php echo $class;?> is-dismissible">
+				        <p><?php echo $message; ?></p>
+				    </div>
+				<?php
+			});
+		}
+	}
+}
+
+/**
+ * Send data to webhooks.
+ *
+ * @param array $data posts data to send to webhooks.
+ *
+ * @since  1.0.0
+ */
+function awp_send_data_to_automate( array $data ) {
+
+	$webhook_url = get_option( 'awp_webhook_url' );
+
+	if ( empty( $webhook_url ) ) {
+		return;
+	}
+
+    $headers 	 = array( 'Accept: application/json', 'Content-Type: application/json' );
+	$args 		 = apply_filters( 'automate_wp_posts_arguments', array(
+					'method'  => 'POST',
+				    'headers' => $headers,
+  	     	        'body'    => json_encode( $data ),
+				));
+
+	$result  = wp_remote_post( esc_url_raw( $webhook_url ), $args );
+
+    if ( is_wp_error( $result ) ) {
+        error_log( print_r( $result->get_error_message(), true ) );
+    }
+
+    do_action( 'automate_wp_posts_data_sent', $result, $webhook_url );
+}
+
+/**
+ * Prepare data to send to webhooks on WP Post Update.
+ * 
+ * @since 1.0.0
+ */
+function awp_publish_post( $id, $post ) {
+
+	$author = $post->post_author;
+	$data = apply_filters( 'automate_wp_posts_data', array(
+		__( 'Post ID', 'automate-wp-posts' ) => $id,
+		__( 'Author Display Name', 'automate-wp-posts' ) => get_the_author_meta( 'display_name', $author ),
+		__( 'Author Email Address', 'automate-wp-posts' ) => get_the_author_meta( 'user_email', $author ),
+		__( 'Post Title', 'automate-wp-posts' ) => $post->post_title,
+		__( 'Post Content', 'automate-wp-posts' ) => $post->post_content,
+		__( 'Post Permalink', 'automate-wp-posts' ) => get_permalink( $id )
+	));
+
+	awp_send_data_to_automate( $data );
+}
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Automate WP Posts With Webhooks ===
+Contributors: sanzeeb3
+Tags: posts, webhooks, zapier, send data,
+Requires at least: 4.0
+Tested up to: 5.2.3
+Requires PHP: 5.3
+Stable tag: 1.0.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Send posts to an online automation tool such as Zapier to automate tasks. 
+
+== Description ==
+
+Send posts to an online automation tool such as Zapier to automate tasks. 
+
+### Features:
+
+* Store WP posts in your Google Spreadsheet.
+* Send Email/SMS alert when a post is published.
+* Share WP posts in social medias such as Facebook, Twitter etc.
+* And many more with [Zapier](https://zapier.com), [automate.io](https://automate.io) and more.
+
+[Documentation](http://sanjeebaryal.com.np/how-to-use-automate-wp-posts-plugin)
+
+Enter your Webhook URL in Settings > Automate WP Posts, that's all.
+
+== Frequently Asked Questions ==
+
+= Nothing is sent? =
+
+Check your debug log. Errors are logged into your debug log. For debugging, see [https://wordpress.org/support/article/debugging-in-wordpress/](https://wordpress.org/support/article/debugging-in-wordpress/)
+
+= Automate WP Posts is awesome. Can I contribute? =
+
+Yes, you can! Join in on [GitHub repository](https://github.com/sanzeeb3/automate-wp-posts).
+
+= Need help? =
+
+[Contact The Developer](http://sanjeebaryal.com.np/contact).
+
+
+== Changelog == 
+
+= 1.0.0 - 10/12/2019 =
+* Initial Release
